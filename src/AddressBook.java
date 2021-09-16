5f5320c9736825594601ceb961db2f88f54fd7d2
Jason <35818579+h-u-a-n-g@users.noreply.github.com>
2021-09-16 19:35:06 -0400

    Git Lab Commit 2

---
+++ src/AddressBook.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddies;
+    public AddressBook(){
+        buddies = new ArrayList<>();
+    }
+    public void addBuddy(BuddyInfo buddy){
+        buddies.add(buddy);
+    }
+
+}
