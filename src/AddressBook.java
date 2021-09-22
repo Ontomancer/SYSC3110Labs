cd2c3d9d66863769cddc05deaf31a76cbb3675ce
Jason <35818579+h-u-a-n-g@users.noreply.github.com>
2021-09-22 16:49:03 -0400

    Git Lab Commit 4

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,17 +6,22 @@ public class AddressBook {
         buddies = new ArrayList<>();
     }
     public void addBuddy(BuddyInfo buddy){
-        buddies.add(buddy);
+        if(buddy != null){
+            buddies.add(buddy);
+        }
     }
-    public void removeBuddy(BuddyInfo buddy){
-        buddies.remove(buddy);
+    public BuddyInfo removeBuddy(int index){
+        if(index >= 0 && index < buddies.size()){
+            return buddies.remove(index);
+        }
+        return null;
     }
     public static void main(String[] args) {
         //System.out.printf("%s", "hello, world!");
         BuddyInfo hs = new BuddyInfo("Homer Simpson", "742 Evergreen Terrace", "+15558675309");
         AddressBook ab = new AddressBook();
         ab.addBuddy(hs);
-        ab.removeBuddy(hs);
-        System.out.println("Git lab output.");
+
+        System.out.println(ab.removeBuddy(0).getName());
     }
 }
